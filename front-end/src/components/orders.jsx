2c342527d6732e410161066f94c2b1b53c328032
Renata <renatasantiago.com@gmail.com>
2023-03-03 15:04:07 -0300

    cria navbar

---
+++ front-end/src/components/orders.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../utils/axiosIstance';
+
+function OrdersCards() {
+  const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
+
+  const getOrders = useCallback(async () => {
+    const userInfo = JSON.parse(localStorage.getItem('User'));
+    try {
+      await axios.get(`orders/${userInfo.id}`).then((response) => {
+        setOrders(response.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  return (
+    <>
+      {
+        orders.map((item) => (
+          <button
+            key={ item.id }
+            onClick={ navigate(`/customer/orders/${item.id}`) }
+            type="button"
+          >
+            <table>
+              <tbody>
+                <tr>
+                  <td
+                    data-testid={ `customer_orders__element-order-id-${item.id}` }
+                  >
+                    {item.id}
+                  </td>
+                  <td
+                    data-testid={ `customer_orders__element-delivery-status-${item.id}` }
+                  >
+                    {item.status}
+                  </td>
+                  <td
+                    data-testid={ `customer_orders__element-order-date-${item.id}` }
+                  >
+                    { item.saleDate }
+                  </td>
+                  <td
+                    data-testid={ `customer_orders__element-card-price-${item.id}` }
+                  >
+                    {item.totalPrice}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </button>
+        ))
+      }
+    </>
+  );
+}
+
+export default OrdersCards;

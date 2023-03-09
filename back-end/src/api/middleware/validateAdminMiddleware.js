575d4501d345d223bad04d5aea2c8cc769b6da06
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-03-09 16:04:35 -0300

    fix: lint

--- back-end/src/api/middleware/validateAdminMiddleware.js
+++ back-end/src/api/middleware/validateAdminMiddleware.js
@@ -12,6 +12,6 @@ module.exports = (req, res, next) => {
     if (data.role !== 'administrator') res.status(401).json({ message: 'Não autorizado' });
     next();
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
